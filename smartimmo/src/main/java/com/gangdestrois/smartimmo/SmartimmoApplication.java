65c31e0a10219bdcdf0927692cd3af6f8572a324
Florine Philippe <fphilippe78480@gmail.com>
2023-10-11 16:57:11 +0200

    chore(init) : initialize project and add properties
    add security
    add tomcat server configuration
    add log level configuration
    add database configuration

---
+++ smartimmo/src/main/java/com/gangdestrois/smartimmo/SmartimmoApplication.java
@@ -0,0 +1,13 @@
+package com.gangdestrois.smartimmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartimmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartimmoApplication.class, args);
+	}
+
+}
