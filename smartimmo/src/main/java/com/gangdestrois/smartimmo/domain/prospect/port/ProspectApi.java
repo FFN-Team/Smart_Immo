635781866ec9437763cfa6b35d66cb6c74594065
FanuelM <103363635+FanuelM@users.noreply.github.com>
2023-12-13 19:22:04 +0100

    Revert "Feature/ti 67 generate statistics for real estate agents"

--- smartimmo/src/main/java/com/gangdestrois/smartimmo/domain/prospect/port/ProspectApi.java
+++ smartimmo/src/main/java/com/gangdestrois/smartimmo/domain/prospect/port/ProspectApi.java
@@ -2,20 +2,11 @@ package com.gangdestrois.smartimmo.domain.prospect.port;
 
 import com.gangdestrois.smartimmo.domain.event.Event;
 import com.gangdestrois.smartimmo.domain.event.EventListener;
-import com.gangdestrois.smartimmo.domain.prospect.model.Prospect;
-import com.gangdestrois.smartimmo.infrastructure.rest.dto.ProspectStatisticsResponse;
 
 import java.util.Set;
-import java.util.List;
-import java.util.Map;
-  
+
 public interface ProspectApi {
     Set<Event> notifyForProspectsThatMayBuyBiggerHouse();
 
     void subscription(EventListener eventListener);
-    
-    List<Prospect> findAll();
-    ProspectStatisticsResponse countByAgeGroup();
-    ProspectStatisticsResponse countByProfession();
-    ProspectStatisticsResponse countByContactOrigin();
 }
--- smartimmo/src/main/java/com/gangdestrois/smartimmo/infrastructure/rest/dto/ProspectDataResponse.java
+++
@@ -1,7 +0,0 @@
-package com.gangdestrois.smartimmo.infrastructure.rest.dto;
-
-import io.soabase.recordbuilder.core.RecordBuilder;
-
-@RecordBuilder
-public record ProspectDataResponse(String label, long value) {
-}
