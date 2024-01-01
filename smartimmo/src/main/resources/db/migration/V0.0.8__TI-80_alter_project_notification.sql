fb4180d1870bbe8acb0ae391ad0bf74fc86b5d7d
Florine Philippe <fphilippe78480@gmail.com>
2024-01-01 14:31:30 +0100

    fix develop

--- smartimmo/src/main/resources/db/migration/V0.0.8__TI-80_alter_project_notification.sql
+++ smartimmo/src/main/resources/db/migration/V0.0.8__TI-80_alter_project_notification.sql
@@ -4,3 +4,28 @@ ALTER TABLE project
 
 ALTER TABLE notification
     ADD COLUMN type VARCHAR(255);
+
+-- Insertion du prospect
+INSERT INTO project (fk_prospect)
+VALUES ((SELECT id_prospect FROM prospect WHERE last_name = 'Durand' AND first_name = 'Marie'));
+
+-- Insertion du projet potentiel
+INSERT INTO potential_project (due_date, fk_project, priority, notification_date)
+VALUES ('2025-02-02', 1, 'HIGH', NOW());
+
+-- Insertion du statut matrimonial
+INSERT INTO home (marital_status)
+VALUES ('COHABITATION');
+
+-- Mise à jour du prospect pour ajouter la clé étrangère vers le domicile
+UPDATE prospect
+SET fk_home = 1
+WHERE last_name = 'Dupont' AND first_name = 'Jean';
+
+-- Insertion du propriétaire
+INSERT INTO owner (active, fk_prospect)
+VALUES (TRUE, (SELECT id_prospect FROM prospect WHERE last_name = 'Dupont' AND first_name = 'Jean'));
+
+-- Insertion du propriétaire de la propriété
+INSERT INTO property_owner (acquisition_date, main, fk_owner)
+VALUES ('2015-06-06', TRUE, 1);
