c317dcbeaa31fd2ef98db070ff170bd0b613c25a
Florine Philippe <fphilippe78480@gmail.com>
2024-01-01 14:51:14 +0100

    fix develop

--- smartimmo/src/main/resources/db/migration/V0.0.8__TI-80_alter_project_notification.sql
+++ smartimmo/src/main/resources/db/migration/V0.0.8__TI-80_alter_project_notification.sql
@@ -7,11 +7,11 @@ ALTER TABLE notification
 
 -- Insertion du prospect
 INSERT INTO project (fk_prospect)
-VALUES ((SELECT id_prospect FROM prospect WHERE last_name = 'Durand' AND first_name = 'Marie'));
+VALUES ((SELECT id_prospect FROM prospect WHERE last_name = 'Durand' AND first_name = 'Marie' LIMIT 1));
 
 -- Insertion du projet potentiel
 INSERT INTO potential_project (due_date, fk_project, priority, notification_date)
-VALUES ('2025-02-02', 1, 'HIGH', NOW());
+VALUES ('2025-02-02', currval('project_id_project_seq'), 'HIGH', NOW());
 
 -- Insertion du statut matrimonial
 INSERT INTO home (marital_status)
@@ -19,13 +19,13 @@ VALUES ('COHABITATION');
 
 -- Mise à jour du prospect pour ajouter la clé étrangère vers le domicile
 UPDATE prospect
-SET fk_home = 1
+SET fk_home = currval('home_id_home_seq')
 WHERE last_name = 'Dupont' AND first_name = 'Jean';
 
 -- Insertion du propriétaire
 INSERT INTO owner (active, fk_prospect)
-VALUES (TRUE, (SELECT id_prospect FROM prospect WHERE last_name = 'Dupont' AND first_name = 'Jean'));
+VALUES (TRUE, (SELECT id_prospect FROM prospect WHERE last_name = 'Dupont' AND first_name = 'Jean' LIMIT 1));
 
 -- Insertion du propriétaire de la propriété
 INSERT INTO property_owner (acquisition_date, main, fk_owner)
-VALUES ('2015-06-06', TRUE, 1);
+VALUES ('2015-06-06', TRUE, currval('owner_id_owner_seq'));
