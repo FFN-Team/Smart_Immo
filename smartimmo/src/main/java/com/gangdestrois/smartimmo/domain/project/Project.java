1970a625b92e534582206953122b02d60bd826b3
Florine Philippe <fphilippe78480@gmail.com>
2024-01-13 12:04:30 +0100

    TI-114 - feature(document management system) : implement Visitor

--- smartimmo/src/main/java/com/gangdestrois/smartimmo/domain/project/Project.java
+++ smartimmo/src/main/java/com/gangdestrois/smartimmo/domain/project/Project.java
@@ -1,7 +1,7 @@
 package com.gangdestrois.smartimmo.domain.project;
 
-import com.gangdestrois.smartimmo.domain.Model;
 import com.gangdestrois.smartimmo.domain.prospect.model.Prospect;
+import com.gangdestrois.smartimmo.domain.tool.Model;
 
 public record Project(Long id, Prospect prospect) implements Model {
 }
--- smartimmo/src/main/java/com/gangdestrois/smartimmo/domain/prospect/model/Prospect.java
+++ smartimmo/src/main/java/com/gangdestrois/smartimmo/domain/prospect/model/Prospect.java
@@ -1,14 +1,14 @@
 package com.gangdestrois.smartimmo.domain.prospect.model;
 
-import com.gangdestrois.smartimmo.domain.Model;
-import com.gangdestrois.smartimmo.domain.event.*;
+import com.gangdestrois.smartimmo.domain.event.Notify;
 import com.gangdestrois.smartimmo.domain.event.enums.EventType;
-import com.gangdestrois.smartimmo.domain.event.enums.Priority;
 import com.gangdestrois.smartimmo.domain.event.enums.NotificationStatus;
+import com.gangdestrois.smartimmo.domain.event.enums.Priority;
 import com.gangdestrois.smartimmo.domain.event.model.Event;
 import com.gangdestrois.smartimmo.domain.prospect.ContactOrigin;
 import com.gangdestrois.smartimmo.domain.prospect.Profession;
 import com.gangdestrois.smartimmo.domain.prospect.Title;
+import com.gangdestrois.smartimmo.domain.tool.Model;
 
 import java.time.LocalDate;
 import java.util.List;
