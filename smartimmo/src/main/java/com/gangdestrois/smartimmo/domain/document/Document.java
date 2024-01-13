8988ddf3496fa88e55aa3830fb3bd98225918d1a
Florine Philippe <fphilippe78480@gmail.com>
2024-01-13 12:04:30 +0100

    TI-114 - feature(document management system) : implement Visitor

--- smartimmo/src/main/java/com/gangdestrois/smartimmo/domain/document/Document.java
+++ smartimmo/src/main/java/com/gangdestrois/smartimmo/domain/document/Document.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 public interface Document {
     String getName();
+    Integer getSize();
     Optional<Folder> getParent();
     Boolean isComposite();
     void accept(DocumentVisitor documentVisitor);
