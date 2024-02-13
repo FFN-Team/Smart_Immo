956a33128144e3042e1703f9725ff6964f801656
FanuelM <40004506@parisnanterre.fr>
2024-02-13 11:32:17 +0100

    TI-118 - test(unit tests): change unit tests for estate agent statistics

--- smartimmo/src/test/java/com/gangdestrois/smartimmo/domain/prospect/ProspectStatisticsGeneratorTest.java
+++ smartimmo/src/test/java/com/gangdestrois/smartimmo/domain/prospect/ProspectStatisticsGeneratorTest.java
@@ -1,98 +1,29 @@
 package com.gangdestrois.smartimmo.domain.prospect;
 
-import com.gangdestrois.smartimmo.domain.filter.prospect.MathematicalComparator;
-import com.gangdestrois.smartimmo.domain.prospect.model.Prospect;
 import com.gangdestrois.smartimmo.domain.prospect.port.ProspectSpi;
+import com.gangdestrois.smartimmo.infrastructure.jpa.ProspectDataAdapter;
 import com.gangdestrois.smartimmo.infrastructure.rest.dto.ProspectDataResponse;
 import com.gangdestrois.smartimmo.infrastructure.rest.dto.ProspectStatisticsResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-class ProspectStatisticsGeneratorTest {
-    @MockBean
+public class ProspectStatisticsGeneratorTest {
     private ProspectStatisticsGenerator prospectStatisticsGenerator;
-
-    class ProspectSpiMock implements ProspectSpi {
-        @Override
-        public List<Prospect> findAll() {
-            return null;
-        }
-
-        @Override
-        public long countByAgeBetween(int ageMin, int ageMax) {
-            if (ageMin == 0) return 5;
-            else if (ageMin == 21) return 10;
-            else if (ageMin == 41) return 15;
-            else if (ageMin == 61) return 20;
-            else return 25;
-        }
-
-        @Override
-        public List<ProspectStatistic> countByProfession() {
-            return List.of(
-                new ProspectStatistic("Doctor", 5),
-                new ProspectStatistic("Engineer", 10),
-                new ProspectStatistic("Teacher", 15),
-                new ProspectStatistic("Student", 20),
-                new ProspectStatistic("Commercial", 25)
-            );
-        }
-
-        @Override
-        public List<ProspectStatistic> countByContactOrigin() {
-            return List.of(
-                new ProspectStatistic("Email", 5),
-                new ProspectStatistic("Phone", 10),
-                new ProspectStatistic("Social Media", 15),
-                new ProspectStatistic("Web Site", 20),
-                new ProspectStatistic("Word of Mouth", 25)
-            );
-        }
-
-        @Override
-        public Optional<Prospect> findById(Long prospectId) {
-            return Optional.empty();
-        }
-
-        @Override
-        public List<Prospect> findAllByAge(Integer age, MathematicalComparator ageComparator) {
-            return null;
-        }
-
-        @Override
-        public List<Prospect> findAllByContactOrigin(ContactOrigin contactOrigin) {
-            return null;
-        }
-
-        @Override
-        public List<Prospect> findAllByTitle(Title title) {
-            return null;
-        }
-
-        @Override
-        public List<Prospect> findAllByProfession(Profession profession) {
-            return null;
-        }
-
-        @Override
-        public List<Prospect> findAllByAuthorizeContactOnSocialMedia(Boolean authorizeContactOnSocialMedia) {
-            return null;
-        }
-    }
+    private ProspectSpi prospectSpi;
 
     @BeforeEach
-    void setUp() {
-        prospectStatisticsGenerator = new ProspectStatisticsGenerator(new ProspectSpiMock());
+    public void setUp() {
+        prospectSpi = mock(ProspectDataAdapter.class);
+        prospectStatisticsGenerator = new ProspectStatisticsGenerator(prospectSpi);
     }
 
     @Test
-    void countByAgeGroup() {
+    public void countByAgeGroup() {
         ProspectStatisticsResponse expectedStatistics = new ProspectStatisticsResponse(
             "Number of prospects by age group",
             List.of(
@@ -103,6 +34,11 @@ class ProspectStatisticsGeneratorTest {
                 new ProspectDataResponse("81 - 120", 25)
             )
         );
+        when(prospectSpi.countByAgeBetween(0, 20)).thenReturn(5L);
+        when(prospectSpi.countByAgeBetween(21, 40)).thenReturn(10L);
+        when(prospectSpi.countByAgeBetween(41, 60)).thenReturn(15L);
+        when(prospectSpi.countByAgeBetween(61, 80)).thenReturn(20L);
+        when(prospectSpi.countByAgeBetween(81, 120)).thenReturn(25L);
 
         ProspectStatisticsResponse actualStatistics = prospectStatisticsGenerator.countByAgeGroup();
 
@@ -110,7 +46,7 @@ class ProspectStatisticsGeneratorTest {
     }
 
     @Test
-    void countByProfession() {
+    public void countByProfession() {
         ProspectStatisticsResponse expectedStatistics = new ProspectStatisticsResponse(
             "Number of prospects by profession",
             List.of(
@@ -121,6 +57,15 @@ class ProspectStatisticsGeneratorTest {
                 new ProspectDataResponse("Commercial", 25)
             )
         );
+        when(prospectSpi.countByProfession()).thenReturn(
+            List.of(
+                new ProspectStatistic("Doctor", 5),
+                new ProspectStatistic("Engineer", 10),
+                new ProspectStatistic("Teacher", 15),
+                new ProspectStatistic("Student", 20),
+                new ProspectStatistic("Commercial", 25)
+            )
+        );
 
         ProspectStatisticsResponse actualStatistics = prospectStatisticsGenerator.countByProfession();
 
@@ -128,7 +73,7 @@ class ProspectStatisticsGeneratorTest {
     }
 
     @Test
-    void countByContactOrigin() {
+    public void countByContactOrigin() {
         ProspectStatisticsResponse expectedStatistics = new ProspectStatisticsResponse(
             "Number of prospects by contact origin",
             List.of(
@@ -137,6 +82,15 @@ class ProspectStatisticsGeneratorTest {
                 new ProspectDataResponse("Social Media", 15),
                 new ProspectDataResponse("Web Site", 20),
                 new ProspectDataResponse("Word of Mouth", 25)
+            )
+        );
+        when(prospectSpi.countByContactOrigin()).thenReturn(
+            List.of(
+                new ProspectStatistic("Email", 5),
+                new ProspectStatistic("Phone", 10),
+                new ProspectStatistic("Social Media", 15),
+                new ProspectStatistic("Web Site", 20),
+                new ProspectStatistic("Word of Mouth", 25)
             )
         );
 
