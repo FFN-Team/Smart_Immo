d14ac3dc240e209be381597466dbd39af4d1fb21
Florine <fphilippe78480@gmail.com>
2024-02-16 17:59:28 +0100

    delete ApiExceptionHandler.java

--- smartimmo/src/main/java/com/gangdestrois/smartimmo/infrastructure/rest/controller/ApiExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.gangdestrois.smartimmo.infrastructure.rest.controller;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class ApiExceptionHandler {
-
-}
