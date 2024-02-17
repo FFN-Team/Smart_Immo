ad7a0fcc98b76be2729929a1d73af08b93445f8e
Florine <fphilippe78480@gmail.com>
2024-02-17 11:04:06 +0100

    TI-114 - feature(create document management system) : add unitary tests and improve tests

---
+++ smartimmo/src/test/java/com/gangdestrois/smartimmo/DataForUT.java
@@ -0,0 +1,8 @@
+package com.gangdestrois.smartimmo;
+
+import com.gangdestrois.smartimmo.domain.document.Folder;
+
+public class DataForUT {
+    public final Folder folder1 = new Folder("test", "test", "test", "test");
+    public final byte[] file = new byte[1];
+}
