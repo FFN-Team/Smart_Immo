12954a3503e9e89b8bce37844dbb8fdc678c3454
FanuelM <40004506@parisnanterre.fr>
2024-03-10 16:24:10 +0100

    TI-108 - refactor: make Period class final and add a private constructor to isolate it and reduce complexity in the isValid method

--- smartimmo/src/main/java/com/gangdestrois/smartimmo/domain/salesHistory/Period.java
+++ smartimmo/src/main/java/com/gangdestrois/smartimmo/domain/salesHistory/Period.java
@@ -5,9 +5,11 @@ import com.gangdestrois.smartimmo.infrastructure.rest.error.ExceptionEnum;
 
 import java.time.LocalDate;
 
-public class Period {
+public final class Period {
     private final static LocalDate LOCAL_DATE_MIN = LocalDate.of(1900, 1, 1);
     private final static Long PERIOD_MAX_IN_YEARS = 100L;
+
+    private Period(){}
 
     private static LocalDate getLocalDateMax(LocalDate startDate) {
         LocalDate potentialLocalDateMax = startDate.plusYears(PERIOD_MAX_IN_YEARS);
@@ -17,28 +19,35 @@ public class Period {
         return potentialLocalDateMax;
     }
 
-    public static boolean endDateBeforeDateMax(LocalDate endDate, LocalDate dateMax) {
+    private static boolean endDateBeforeDateMax(LocalDate startDate, LocalDate endDate) {
+        LocalDate dateMax = getLocalDateMax(startDate);
         return endDate.isBefore(dateMax) || endDate.isEqual(dateMax);
     }
 
-    public static boolean isValid(LocalDate startDate, LocalDate endDate) {
-        boolean startBeforeEnd = startDate.isBefore(endDate) || startDate.isEqual(endDate);
-        boolean startAfterDateMin = startDate.isEqual(LOCAL_DATE_MIN) || startDate.isAfter(LOCAL_DATE_MIN);
-        boolean periodInYearsValid = java.time.Period.between(startDate, endDate).getYears() <= PERIOD_MAX_IN_YEARS;
-        LocalDate dateMax = getLocalDateMax(startDate);
-        boolean endBeforeDateMax = endDateBeforeDateMax(endDate, dateMax);
+    private static boolean startBeforeEnd(LocalDate startDate, LocalDate endDate) {
+        return startDate.isBefore(endDate) || startDate.isEqual(endDate);
+    }
 
-        if (!startBeforeEnd) throw new BadRequestException(ExceptionEnum.BAD_REQUEST,
+    private static boolean startAfterDateMin(LocalDate startDate) {
+        return startDate.isEqual(LOCAL_DATE_MIN) || startDate.isAfter(LOCAL_DATE_MIN);
+    }
+
+    private static boolean periodInYearsValid(LocalDate startDate, LocalDate endDate) {
+        return java.time.Period.between(startDate, endDate).getYears() <= PERIOD_MAX_IN_YEARS;
+    }
+
+    public static boolean isValid(LocalDate startDate, LocalDate endDate) {
+        if (!startBeforeEnd(startDate, endDate)) throw new BadRequestException(ExceptionEnum.BAD_REQUEST,
                 "The start date must be before the end date.");
 
-        if (!startAfterDateMin) throw new BadRequestException(ExceptionEnum.BAD_REQUEST,
+        if (!startAfterDateMin(startDate)) throw new BadRequestException(ExceptionEnum.BAD_REQUEST,
                 String.format("The start date must be after %s.", LOCAL_DATE_MIN));
 
-        if (!periodInYearsValid) throw new BadRequestException(ExceptionEnum.BAD_REQUEST,
+        if (!periodInYearsValid(startDate, endDate)) throw new BadRequestException(ExceptionEnum.BAD_REQUEST,
                 String.format("The period must be less than %d years.", PERIOD_MAX_IN_YEARS));
 
-        if (!endBeforeDateMax) throw new BadRequestException(ExceptionEnum.BAD_REQUEST,
-                String.format("The end date must be before %s.", dateMax));
+        if (!endDateBeforeDateMax(startDate, endDate)) throw new BadRequestException(ExceptionEnum.BAD_REQUEST,
+                String.format("The end date must be before %s.", getLocalDateMax(startDate)));
 
         return true;
     }
